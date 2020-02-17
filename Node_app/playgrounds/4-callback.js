3f43ce6211568649f3cf4955ef5fb3025fc00a97
joyaAmora <jade.phaneuf@gmail.com>
2020-02-17 11:56:00 -0500

    weather part 2

---
+++ Node_app/playgrounds/4-callback.js
@@ -0,0 +1,34 @@
+// setTimeout(() => {
+//     console.log('two seconds time out')
+// }, 2000)
+
+// const names = ['Andrew', 'Jen', 'Jess']
+// const shortNames = names.filter((name) => {
+//     return name.length <=4
+// })
+
+// const geocode = (address, callback) => {
+//     setTimeout(() => {
+//         const data = {
+//             lat: 0,
+//             long: 0,
+//         }
+//         callback(data)
+//     }, 2000)
+// }
+
+// geocode('Philadelphia', (data) => {
+//     console.log(data)
+// })
+
+
+const add = (1,4, (a, b, callback) => {
+    setTimeout(() => {
+        console.log('two seconds time out')
+        callback(a+b)
+    }, 2000)
+})
+
+add (1,4, (sum) => {
+    console.log(sum)
+})
--- nodes_app/weater-app/app.js
+++ nodes_app/weater-app/app.js
@@ -1,4 +1,5 @@
 const request = require('request')
+const geocode = require('./utils/geocode')
 
 // const url = 'https://api.darksky.net/forecast/18981b1f9bc4d9e2838e9cf103bc29ba/37.8267,-122.4233?units=si&lang=fr'
 
@@ -17,18 +18,25 @@ const request = require('request')
     
 // }) 
 
-const url1 = 'https://api.mapbox.com/geocoding/v5/mapbox.places/shawinigan.json?access_token=pk.eyJ1Ijoiam95YWFtb3JhIiwiYSI6ImNrNnBwdGw5YzFtc2MzbG83MWZubGc0bnIifQ.SuQ2LhaLcLiA7k78f4tP2Q'
+// const url1 = 'https://api.mapbox.com/geocoding/v5/mapbox.places/shawinigan.json?access_token=pk.eyJ1Ijoiam95YWFtb3JhIiwiYSI6ImNrNnBwdGw5YzFtc2MzbG83MWZubGc0bnIifQ.SuQ2LhaLcLiA7k78f4tP2Q'
 
-request({ url: url1, json: true }, (error, response) => {
-    if(error)
-        console.log('Unable to connect to weather service!')
-    else if (response.body.features.length === 0)
-        console.log('Unable to find location')
-    else
-    {
-        const latitude = response.body.features[0].center[1]
-        const longitude = response.body.features[0].center[0]
-        console.log(latitude, longitude)
-    }
+// request({ url: url1, json: true }, (error, response) => {
+//     if(error)
+//         console.log('Unable to connect to weather service!')
+//     else if (response.body.features.length === 0)
+//         console.log('Unable to find location')
+//     else
+//     {
+//         const latitude = response.body.features[0].center[1]
+//         const longitude = response.body.features[0].center[0]
+//         console.log(latitude, longitude)
+//     }
 
+// })
+
+
+
+geocode('Boston', (error, data) => {
+    console.log('error', error)
+    console.log('data', data)
 })
