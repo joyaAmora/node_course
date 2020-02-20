a01f62ee800a69eac89a5e6767a042b0df59cdc9
Jade <jade.phaneuf@gmail.com>
2020-02-19 19:25:19 -0500

    destructuring

--- nodes_app/weater-app/utils/forecast.js
+++ nodes_app/weater-app/utils/forecast.js
@@ -2,16 +2,16 @@ const request = require('request')
 
 const forecast = (lat, long, callback) => {
     const url = 'https://api.darksky.net/forecast/18981b1f9bc4d9e2838e9cf103bc29ba/' + lat + ',' + long
-    request({url: url, json: true}, (error, response) => {
+    request({url, json: true}, (error, {body}) => {
         if(error)
             callback('Unable to connect to location service', undefined)
-        else if(response.body.error){
+        else if(body.error){
             callback('Unable to fin location try other coordinates', undefined)
         }
         else
             callback(undefined, 
-                response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degrees and there is ' + 
-                response.body.currently.precipProbability + ' % of rain'
+                body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees and there is ' + 
+                body.currently.precipProbability + ' % of rain'
             )
     })
 }
